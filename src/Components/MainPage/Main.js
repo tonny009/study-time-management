b3420735935e9022e43c2ad20e8f3d67856e010d
tonny009 <tabassum.islam009@gmail.com>
2022-09-29 22:16:59 +0900

    pictures updated

--- src/Components/MainPage/Main.js
+++ src/Components/MainPage/Main.js
@@ -6,13 +6,13 @@ import './Main.css';
 
 const Main = () => {
     const subjects = [
-        { id: 1, name: 'Physics 2nd paper', time: 2, author: 'Dr. Shahjahan Tapan', instructions: 'Read any 2 lessons with full mcq and cq practises.', image: 'images/physics1st.jpg' },
-        { id: 2, name: 'Physics 1st paper', time: 1, author: 'Dr. Amir Hossain khan', instructions: 'Read any 1 lesson with full mcq and cq practises.', image: 'images/physics1st.jpg' },
-        { id: 3, name: 'Chemistry 1st paper', time: 2, author: 'Dr. Saroj Kanti Sign Hazari', instructions: 'Complete 3 lessons with practising all reaction terms.', image: 'images/physics1st.jpg' },
-        { id: 4, name: 'Chemistry 2nd paper', time: 1., author: 'Dr. Sanjit Kumar Guha', instructions: 'Complete 1 lessons with al chemical reactions.', image: 'images/physics1st.jpg' },
-        { id: 5, name: 'Math 1st paper', time: 3, author: 'MA Jobbar', instructions: 'Practice any 2 lessons with exercise and example problem.', image: 'images/physics1st.jpg' },
-        { id: 6, name: 'Math 2nd paper', time: 2, author: 'MA Jobbar', instructions: 'Practice important exercise and example problem.', image: 'images/physics1st.jpg' },
-        { id: 7, name: 'English 1st paper', time: 2, author: 'Board Book', instructions: 'Read any 6 lessons with model question solving.', image: 'images/physics1st.jpg' }
+        { id: 1, name: 'Physics 1st paper', time: 2, author: 'Dr. Shahjahan Tapan', instructions: 'Read any 2 lessons with full mcq and cq practises.', image: 'images/physics1st.jpg' },
+        { id: 2, name: 'Physics 2nd paper', time: 1, author: 'Dr. Amir Hossain khan', instructions: 'Read any 1 lesson with full mcq and cq practises.', image: 'images/physics2nd.jpg' },
+        { id: 3, name: 'Chemistry 1st paper', time: 2, author: 'Dr. Saroj Kanti Sign Hazari', instructions: 'Complete 3 lessons with practising all reaction terms.', image: 'images/chem1st.jpg' },
+        { id: 4, name: 'Chemistry 2nd paper', time: 1., author: 'Dr. Sanjit Kumar Guha', instructions: 'Complete 1 lessons with al chemical reactions.', image: 'images/chem2nd.jpg' },
+        { id: 5, name: 'Math 1st paper', time: 3, author: 'MA Jobbar', instructions: 'Practice any 2 lessons with exercise and example problem.', image: 'images/math1s.jpg' },
+        { id: 6, name: 'Math 2nd paper', time: 2, author: 'MA Jobbar', instructions: 'Practice important exercise and example problem.', image: 'images/math2nd.jpg' },
+        { id: 7, name: 'English 1st paper', time: 2, author: 'Board Book', instructions: 'Read any 6 lessons with model question solving.', image: 'images/eng1st.jpg' }
     ]
 
     const [list, setList] = useState([]);
